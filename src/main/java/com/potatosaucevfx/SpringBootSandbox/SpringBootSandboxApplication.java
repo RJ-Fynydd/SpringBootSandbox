6f29ee086bd3109ab2cec1b190199eb5d691fbcd
Richard Nader <richard.nader@fynydd.com>
2017-11-15 20:02:43 -0500

    Set up some basics

--- src/main/java/com/potatosaucevfx/SpringBootSandbox/SpringBootSandboxApplication.java
+++ src/main/java/com/potatosaucevfx/SpringBootSandbox/SpringBootSandboxApplication.java
@@ -9,4 +9,5 @@ public class SpringBootSandboxApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSandboxApplication.class, args);
 	}
+	
 }
---
+++ src/main/java/com/potatosaucevfx/SpringBootSandbox/config/MvcConfig.java
@@ -0,0 +1,28 @@
+package com.potatosaucevfx.SpringBootSandbox.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+	}
+	
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/*.css/**").addResourceLocations("classpath:/static/css/");
+		registry.addResourceHandler("/*.js/**").addResourceLocations("classpath:/static/js/");
+		registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+	}
+	
+	
+	
+}
