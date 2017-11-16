19289835f5c98a215c708b6dfba7e2327c8a1f45
Richard Nader <richard.nader@fynydd.com>
2017-11-15 21:02:22 -0500

    Added some basic SCSS.

--- src/main/java/com/potatosaucevfx/SpringBootSandbox/config/MvcConfig.java
+++ src/main/java/com/potatosaucevfx/SpringBootSandbox/config/MvcConfig.java
@@ -12,7 +12,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/home").setViewName("home");
 		registry.addViewController("/").setViewName("home");
-		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/welcome").setViewName("welcome");
 		registry.addViewController("/login").setViewName("login");
 	}
 	
